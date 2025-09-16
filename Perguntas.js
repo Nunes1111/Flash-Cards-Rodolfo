f620904ae4ac0078df520bb556a6ce609768cae0
Nunes1111 <nunes.rodolfo@escola.pr.gov.br>
2025-09-16 09:22:48 -0300

    16/09

--- Perguntas.js
+++ Perguntas.js
@@ -1,11 +1,59 @@
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Conhecimentos Gerais ',
+    'Qual é o maioir planeta do nosso sitema solar?',
+    'Jupiter'
 )
 
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Conhecimentos Gerais',
+    'Qual é o animal terrestre mais rápido ',
+    'O Guepardo'
+)
+
+criaCartao(
+    'Conhecimentos Gerais ',
+    'Qual a capital do Japão',
+    'Tóquio'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quantos ossos tem um esqueleto humano adulto?',
+    '206 ossos'
+)
+
+criaCartao(
+    'Conhecimentos Gerais ',
+    'Quem pitou a mona lisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é i elemento  químico mais abundante no universo? ',
+    'Hidrogênio'
+)
+
+criaCartao(
+    'Conhecimentos Gerais ',
+    'Qual é oceano mais porfundo da Terra?',
+    'Oceano Pacífico'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é a maior pirâmede do mundo?',
+    'A Grande Pirâmide de Gizé, no Egito'
+)
+
+criaCartao(
+    'Conhecimentos Gerais ',
+    'Qual é o metal mais caro do mundo?',
+    'Ródio'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Onde fica a GRande Barreira de Coral?',
+    'NA costa da Austrália'
 )
